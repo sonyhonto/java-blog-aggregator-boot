7c818022ca3c7c221f6c516f044d4ac308c77bd5
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-02-06 12:38:59 +0100

    simple controller get by id
    get by id endpoint

--- src/main/java/cz/jiripinkas/jba/simple/SimpleController.java
+++ src/main/java/cz/jiripinkas/jba/simple/SimpleController.java
@@ -1,11 +1,13 @@
 package cz.jiripinkas.jba.simple;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,19 @@ public class SimpleController {
     @GetMapping("/all")
     public ResponseEntity<List<SimpleEntity>> all() {
         return ResponseEntity.ok(this.simpleService.all());
+    }
+
+    @RequestMapping("/{requestedId}")
+    public ResponseEntity<SimpleEntity> selectTheName(@PathVariable("requestedId") Integer id) {
+
+        Optional<SimpleEntity> entity = this.simpleService.findById(id);
+
+        if (entity.isPresent()) {
+            return ResponseEntity.ok(entity.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
     @PostMapping("/create")
